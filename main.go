1931a96fdccb755e218ef86122292dc6367f9730
[FadyGamilM] <1700961@eng.asu.edu.eg>
2023-10-27 16:55:38 +0200

    K8s | first deployment

--- main.go
+++ main.go
@@ -1,51 +1,40 @@
 package main
 
 import (
-	"godocker/database"
-	"io/ioutil"
+	"godocker/transport"
 	"log"
 	"os"
-	"time"
-
-	"github.com/gin-gonic/gin"
 )
 
-func init() {
-	databaseUrl := os.Getenv("DATABASE_URL")
-	if databaseUrl == "" {
-		content, err := ioutil.ReadFile(os.Getenv("DATABASE_URL_FILE"))
-		if err != nil {
-			log.Fatal(err)
-		}
-		databaseUrl = string(content)
-	}
+// func init() {
+// 	databaseUrl := os.Getenv("DATABASE_URL")
+// 	if databaseUrl == "" {
+// 		content, err := ioutil.ReadFile(os.Getenv("DATABASE_URL_FILE"))
+// 		if err != nil {
+// 			log.Fatal(err)
+// 		}
+// 		databaseUrl = string(content)
+// 	}
 
-	errDB := database.InitDB(databaseUrl)
-	if errDB != nil {
-		log.Fatalf("⛔ Unable to connect to database: %v\n", errDB)
-	} else {
-		log.Println("DATABASE CONNECTED 🥇")
-	}
+// 	errDB := database.InitDB(databaseUrl)
+// 	if errDB != nil {
+// 		log.Fatalf("⛔ Unable to connect to database: %v\n", errDB)
+// 	} else {
+// 		log.Println("DATABASE CONNECTED 🥇")
+// 	}
 
-}
+// }
 
 func main() {
 
-	r := gin.Default()
-	var tm time.Time
+	router := transport.CreateRouter()
 
-	r.GET("/", func(c *gin.Context) {
-		tm = database.GetTime(c)
-		c.JSON(200, gin.H{
-			"api": "golang server",
-			"now": tm,
-		})
-	})
+	PORT := os.Getenv("SERVER_PORT")
+	if PORT == "" {
+		log.Println("⛔ PORT IS NOT PASSED ⛔")
+		PORT = "5000"
+	}
 
-	r.GET("/ping", func(c *gin.Context) {
-		tm = database.GetTime(c)
-		c.JSON(200, "pong")
-	})
-
-	r.Run() // listen and serve on 0.0.0.0:8080 (or "PORT" env var)
+	server := transport.CreateServer(router, PORT)
+	transport.InitServer(server)
 }
---
+++ transport/server.go
@@ -0,0 +1,30 @@
+package transport
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+func CreateRouter() *gin.Engine {
+	return gin.Default()
+}
+
+func CreateServer(r *gin.Engine, PORT string) *http.Server {
+	return &http.Server{
+		Addr:    PORT,
+		Handler: r,
+	}
+}
+
+func InitServer(srv *http.Server) {
+	if err := srv.ListenAndServe(); err != nil {
+		log.Println("failed initiating the server")
+		os.Exit(1)
+	} else {
+
+		log.Println("🥇 Server is up and running ..")
+	}
+}
